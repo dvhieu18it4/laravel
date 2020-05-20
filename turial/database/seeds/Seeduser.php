4400eb9c14d0513ad22fa63689d6e551c43e43d7
dvhieu18it4 <44391096+dvhieu18it4@users.noreply.github.com>
2020-05-20 22:03:44 +0700

    Create Seeduser.php

---
+++ turial/database/seeds/Seeduser.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\DB;
+use App\lichtrinh;
+use App\CTchuyendi;
+use App\User;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {   
+         $time = date('Y-m-d H:i:s');
+   			User::create([
+   				'id' =>'1',
+          'xe_id'=>'',
+          'name' =>'Admin',
+          'phone' =>'9999999',
+          'usertype'=>'admin',
+          'email' =>'admin@me.com',
+          'password' =>'123456',
+          
+    
+   			]);   
+         
+    }
+}
