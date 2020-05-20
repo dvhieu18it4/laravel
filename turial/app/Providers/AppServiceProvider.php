1f8a902b4889f3f2df7279df48e59faf1608a927
dvhieu18it4 <44391096+dvhieu18it4@users.noreply.github.com>
2020-05-20 15:58:33 +0700

    Update AppServiceProvider.php

--- turial/app/Providers/AppServiceProvider.php
+++ turial/app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
  
-   
+    
     public function boot()
     {
-  
+         URL::forceScheme('https');
     }
 }
